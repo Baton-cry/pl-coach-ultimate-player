68643faa98f5a84ab3c5ba40fc122ddc5d2da754
Baton-cry <cezarfahtovik@gmail.com>
2026-02-14 16:14:26 +0300

    complete work

--- vite.config.ts
+++ vite.config.ts
@@ -7,25 +7,31 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg','icons/icon-192.png','icons/icon-512.png'],
+
+      includeAssets: ['favicon.svg', 'icons/icon-192.png', 'icons/icon-512.png'],
+
       manifest: {
         name: 'PL Coach IDEAL',
         short_name: 'PL IDEAL',
         description: 'Пауэрлифтинг: план, замены, анатомия, чек-ин, КБЖУ. Офлайн + пароль.',
-        theme_color: '#07070a',
-        background_color: '#07070a',
+        theme_color: '#00707a',
+        background_color: '#00707a',
         display: 'standalone',
-        start_url: './',
+        start_url: '/',
         icons: [
-          { src: './icons/icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: './icons/icon-512.png', sizes: '512x512', type: 'image/png' }
+          { src: '/icons/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/icons/icon-512.png', sizes: '512x512', type: 'image/png' },
         ],
       },
+
       workbox: {
         navigateFallback: 'index.html',
+        // важно: чтобы SW НЕ подменял /api/* на index.html
+        navigateFallbackDenylist: [/^\/api\//],
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-      }
-    })
+      },
+    }),
   ],
-  server: { port: 5173 }
+
+  server: { port: 5173 },
 })
